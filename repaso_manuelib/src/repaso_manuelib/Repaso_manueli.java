bd4d1a494b634195bee957d24d8f1e0f3cc50420
Manuel <Manuel@DESKTOP-manu>
2025-05-12 09:14:12 +0200

    commit prueba

--- repaso_manuelib/src/repaso_manuelib/Repaso_manueli.java
+++ repaso_manuelib/src/repaso_manuelib/Repaso_manueli.java
@@ -3,7 +3,7 @@ package repaso_manuelib;
 public class Repaso_manueli {
 
 	public static void main(String[] args) {
-		System.out.println("repaso manuel ibanez ");
+		System.out.println("repaso manuel ibanez  ");
 
 	}
 
